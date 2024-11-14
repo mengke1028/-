0cbe19bb9b1d217a9000f95afd98e8327dda5b7c
keke <mengke1028@163.com>
2024-11-14 15:58:51 +0800

    删除python虚拟环境

--- venv/Lib/site-packages/tidevice/_compat.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""Created on Fri Dec 17 2021 14:55:55 by codeskyblue
-"""
-
-from functools import lru_cache
-
-cache = lru_cache(maxsize=None)
--- venv/Lib/site-packages/weditor/web/version.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-#
-
-try:
-    import pkg_resources
-    __version__ = pkg_resources.get_distribution("weditor").version
-except pkg_resources.DistributionNotFound:
-    __version__ = "unknown"
